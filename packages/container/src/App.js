95dfd434043282cbbbe0a26e9b208217522972cf
Yoelvis Mulen <ymulenll@gmail.com>
2020-12-14 15:12:22 -0300

    Update text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <div>Hi There!</div>
+      <div>Hi there!</div>
       <hr />
       <MarketingApp />
     </div>
